40a80be786514c50fb8497e67dcc3bce9fbb1a16
mahowik56 <75947015+mahowik56@users.noreply.github.com>
2025-06-05 11:07:00 +0300

    Extend user persistence

--- UTanksServer/ECS/Systems/User/UserCrystalsPersistenceSystem.cs
+++ UTanksServer/ECS/Systems/User/UserCrystalsPersistenceSystem.cs
@@ -5,6 +5,8 @@ using UTanksServer.ECS.Components.User;
 using UTanksServer.ECS.ECSCore;
 using UTanksServer.Extensions;
 using UTanksServer.Database.Databases;
+using UTanksServer;
+using Newtonsoft.Json;
 
 namespace UTanksServer.ECS.Systems.User
 {
@@ -26,6 +28,63 @@ namespace UTanksServer.ECS.Systems.User
                         return;
                     var username = entity.GetComponent<UsernameComponent>().Username;
                     _ = UserDatabase.Users.UpdateCrystallsAsync(username, crystals.UserCrystals);
+                }
+            });
+
+            ComponentsOnChangeCallbacks.Add(UserRankComponent.Id, new List<Action<ECSEntity, ECSComponent>>()
+            {
+                (entity, component) =>
+                {
+                    if (component is not UserRankComponent rank)
+                        return;
+                    var username = entity.GetComponent<UsernameComponent>().Username;
+                    _ = UserDatabase.Users.UpdateRankAsync(username, rank.Rank);
+                }
+            });
+
+            ComponentsOnChangeCallbacks.Add(UserScoreComponent.Id, new List<Action<ECSEntity, ECSComponent>>()
+            {
+                (entity, component) =>
+                {
+                    if (component is not UserScoreComponent score)
+                        return;
+                    var username = entity.GetComponent<UsernameComponent>().Username;
+                    _ = UserDatabase.Users.UpdateScoreAsync(username, score.GlobalScore, score.RankScore);
+                }
+            });
+
+            ComponentsOnChangeCallbacks.Add(UserLocationComponent.Id, new List<Action<ECSEntity, ECSComponent>>()
+            {
+                (entity, component) =>
+                {
+                    if (component is not UserLocationComponent location)
+                        return;
+                    var username = entity.GetComponent<UsernameComponent>().Username;
+                    _ = UserDatabase.Users.UpdateLocationAsync(username, location.UserLocation);
+                }
+            });
+
+            ComponentsOnChangeCallbacks.Add(UserKarmaComponent.Id, new List<Action<ECSEntity, ECSComponent>>()
+            {
+                (entity, component) =>
+                {
+                    if (component is not UserKarmaComponent karma)
+                        return;
+                    var username = entity.GetComponent<UsernameComponent>().Username;
+                    _ = UserDatabase.Users.UpdateKarmaAsync(username, karma.UserKarma);
+                }
+            });
+
+            ComponentsOnChangeCallbacks.Add(UserGarageDBComponent.Id, new List<Action<ECSEntity, ECSComponent>>()
+            {
+                (entity, component) =>
+                {
+                    if (component is not UserGarageDBComponent garage)
+                        return;
+                    var username = entity.GetComponent<UsernameComponent>().Username;
+                    // The component contains structured data; store JSON
+                    var json = JsonConvert.SerializeObject(garage);
+                    _ = UserDatabase.Users.UpdateGarageAsync(username, json);
                 }
             });
         }
